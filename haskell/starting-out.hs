1021a9dca9356ae06ff50a5134e9bcbd9304d1c1
Florin-Gabriel Barbuceanu <Floryn2009@windowslive.com>
2016-10-23 15:40:33 +0300

    Reached fst/snd in tuples.

--- haskell/starting-out.hs
+++ haskell/starting-out.hs
@@ -1,11 +1,20 @@
 doubleMe x = x + x
 
 -- doubleUs x y = x + x + y + y
-
 doubleUs x y = doubleMe x + doubleMe y
 
 doubleSmallNumber x = if x > 100
     then x
     else x * 2
 
-doubleAndIncrementedSmallNumber x = (if x > 100 then x else x * 2) + 1 -- if parantheses were omitted, the one would be added only on the else branch.
+-- if parantheses were omitted, the one would be added only on the else branch.
+doubleAndIncrementedSmallNumber x = (if x > 100 then x else x * 2) + 1
+
+combineWords nouns adjectives = [adjective ++ " " ++ noun | noun <- nouns, adjective <- adjectives]
+
+length' xs = sum [1 | _ <- xs]
+
+-- taking advantage of the fact that strings are lists as well.
+removeNonUppercase xs = [x | x <- xs, x `elem` ['A' .. 'Z']]
+
+removeOdds nestedlists = [[x | x <- lst, even x] | lst <- nestedlists]
