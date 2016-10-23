c1cb2615c85280cd737c9493295f8b620d939657
Florin-Gabriel Barbuceanu <Floryn2009@windowslive.com>
2016-10-23 13:38:28 +0300

    Reached cycle/repeat, right before list comprehensions.

--- haskell/starting-out.hs
+++ haskell/starting-out.hs
@@ -8,4 +8,4 @@ doubleSmallNumber x = if x > 100
     then x
     else x * 2
 
-doubleSmallNumberIncremented x = (if x > 100 then x else x * 2) + 1 -- if parantheses were omitted, the one would be added only on the else branch.
+doubleAndIncrementedSmallNumber x = (if x > 100 then x else x * 2) + 1 -- if parantheses were omitted, the one would be added only on the else branch.
